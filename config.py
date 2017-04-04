d2a2123ab6a4f74972a737d5ebf47a0cd7c38587
Matthew Payne <mpayne@bannerengineering.com>
2017-04-04 13:24:06 -0500

    Updated config variables fo deployment

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 # user for each installation.
 
 # COM_PORT defines the serial port used. On Windows this will be 'COM#'.
-COM_PORT = 'COM5'
+COM_PORT = 'COM3'
 
 # Set to false for normal use (fullsceen). Set to true when you want the GUI
 # to launch as a floating window (e.g. development using a screen with a
 # higher resolution than the deployed notebook).
-DEV_MODE = True
+DEV_MODE = False
 
 # The screen resolution in the final deployment.
 SCREEN_RES = (1366, 768)
